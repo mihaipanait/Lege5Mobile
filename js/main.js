ab597da8e23fa72aac370796b8d36a3b81361dbb
Mihai Panait <panait_mihai@yahoo.com>
2014-09-08 17:18:20 +0300

    increased slider duration even more.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $('.tooltip-social').tooltip({
 $(document).ready(function($) {
 	$('#main-slider').flexslider({
 		animation: "fade",
-		slideshowSpeed: 4500,
+		slideshowSpeed: 5000,
 		controlNav: false,
 		directionNav: false
 	});
